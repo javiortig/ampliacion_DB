557df03422721190e32d8ddb8677613c07307f22
Ekaitz723 <52826804+Ekaitz723@users.noreply.github.com>
2021-10-19 11:35:37 +0200

    Actualizo ModelCursor
    añado __iter__, arreglo alive(), sigue dando error next ya que Model no recoge bien el diccionario

--- practica-1/Models/ModelCursor.py
+++ practica-1/Models/ModelCursor.py
@@ -1,11 +1,12 @@
+from pymongo import command_cursor
 
 class ModelCursor:
     """ Cursor para iterar sobre los documentos del resultado de una
     consulta. Los documentos deben ser devueltos en forma de objetos
     modelo.
     """
-    model_class = None
-    command_cursor = None
+#     model_class = None
+#     command_cursor = None
     def __init__(self, model_class, command_cursor):
         """ Inicializa ModelCursor
         Argumentos:
@@ -17,17 +18,21 @@ class ModelCursor:
         self.command_cursor = command_cursor
         pass #No olvidar eliminar esta linea una vez implementado
     
+    def __iter__(self):
+        return self.model_class.__init__(self.command_cursor)
+    
     def next(self):
         """ Devuelve el siguiente documento en forma de modelo
         """
         if(self.alive()):
-            return self.model_class.__init__(self.command_cursor.next())
+            return self.model_class.__init__(self.command_cursor)
         else:
             return None
+        self.command_cursor.next()
 
     @property
     def alive(self):
         """True si existen más modelos por devolver, False en caso contrario
         """
-        return self.command_cursor.alive()
+        return self.command_cursor.alive
         pass #No olvidar eliminar esta linea una vez implementado
