da73a8f24d1a2f865c12e10dce599ff873cefb01
Ekaitz723 <52826804+Ekaitz723@users.noreply.github.com>
2021-11-09 09:10:47 -0800

    ahora si
    intenta forzarmele un error en la ejecucion >:u
    (na seguramente algo me deje sin asegurar xd)

--- practica-2/sesion.py
+++ practica-2/sesion.py
@@ -1,80 +1,112 @@
 import redis
 import json
 import secrets
+import random
 from typing import Union
 import constants.redis as rdK
 
 class Sesion:
-    data_keys = ['username','name','password','privilege']
-    sesion = None
+	data_keys = {'username','name','password','privilege'}
+	sesion = None
 
-    def createSesion(self,privilege=0,**kwargs) -> Union[dict,None] :
-        # Add 'privilege' and check that the data is correct before dumping it in self
-        kwargs['privilege'] = privilege
-        self._filter(**kwargs)
-        self.__dict__.update(kwargs)
-        
-        #Create the token
-        token = secrets.token_hex(16)
+	def __init__(self):
+		self.__class__._init_class()
 
-        # It saves it on the redis server. If it already existed, it does not change it. 
-        res = self.sesion.setnx(token,json.dumps({k:v for k,v in self.__dict__.items() if k in self.data_keys}))
-        
-        # In case it already exists, it returns -1. 
-        if res == 0:
-            return 1
-        # If it exists, it returns token and privilege.
-        else:
-            self.sesion.expire(token,rdK.SESION_EXPIRATION_TIME)
-            return {'token':token, 'privilege':self.__dict__['privilege']}
-        
-    #Para comprobar que funciona bien despues borrar(?)
-    def verDatosUsuario(self) -> Union[dict,None] :
-        #solo se comprueba username ya que si esta username, tambien estan el resto
-        if 'username' in self.__dict__:
-            return {k:v for k,v in self.__dict__.items() if k in self.data_keys}
-        return None
-    
-    def loadUserData(self,token):
-        # Loads user data from the server and dumps it into self
-        userData = json.loads(self.sesion.get(token))
-        self.__dict__.update(userData)
+	def createSesion(self,**kwargs) -> Union[dict,None] :
+		# Add 'privilege' if does not have it and check that the data is correct before dumping it in self
+		if 'privilege' not in kwargs.keys():
+			kwargs['privilege'] = 0
+		self._filter(**kwargs)
+		self.__dict__.update(kwargs)
 
-        return userData
+		#Create the token
+		token = secrets.token_hex(16)
 
-    def updateUserData(self,token="",**kwargs) -> None :
-        # Checks that we have passed a correct token to it
-        if token=="":
-            return None
+		# It saves it on the redis server. If it already existed, it does not change it. 
+		res = self.sesion.setnx(token,json.dumps({k:v for k,v in self.__dict__.items() if k in self.data_keys}))
 
-        # Saves the remaining ttl of the session
-        # If it is -1 it means that it did not exist
-        ttlSesion = self.sesion.ttl(token)
-        if ttlSesion==-1:
-            return None
-        
-        # Update the data, and put the ttl back in
-        self.sesion.set(token,self.loadUserData())
-        self.sesion.expire(token,ttlSesion)
-    
-    def loginToken(self,token) -> int :
-        # Collect the 'privilege' field of the session. 
-        # If it does not exist, returns -1
-        res = self.sesion.get(token,'privilege')
-        if res is None:
-            res = -1
-        return int(res)
+		# In case it already exists, it returns -1. 
+		if res == 0:
+			return -1
+		# If not, it returns token and privilege.
+		else:
+			self.sesion.expire(token,rdK.SESION_EXPIRATION_TIME)
+			return {'token':token, 'privilege':self.__dict__['privilege']}
 
-    def _filter(self, **kwargs) -> bool:
-        args_set = set(kwargs.keys())
+	# Check local data
+	def verDatosUsuario(self) -> Union[dict,None] :
+		#solo se comprueba username ya que si esta username, tambien estan el resto
+		if 'username' in self.__dict__:
+			return {k:v for k,v in self.__dict__.items() if k in self.data_keys}
+		return None
 
-        # with set theory we check that |kwargs ∩ data_keys|=|data_keys|
-        if (len(args_set & self.data_keys) != len(self.data_keys)):
-            raise Exception('missing required variables on argument')
-        return True
+	def loadUserData(self,token):
+		# Loads user data from the server and dumps it into self
+		res = self.sesion.get(token)
+		if res is None:
+			return -1
+		userData = json.loads(res)
+		self.__dict__.update(userData)
 
-    @classmethod
-    def _init_class(cls):
-        cls.sesion = redis.Redis(host=rdK.DB_ADDRESS, port= rdK.DB_PORT, db=rdK.DB_SESION_INDEX)
-        cls.sesion.config_set(rdK.REDIS_MAXMEM_STR, rdK.MAXMEM)
-        cls.sesion.config_set(rdK.REDIS_POLICY_STR, rdK.POLICY)
+		return userData
+
+	def updateUserData(self,token="",**kwargs) -> None :
+		self._filter(**kwargs)
+		
+		# Checks that we have passed a correct token to it
+		if token=="":
+			return None
+
+		# Update the data, and put the ttl back in
+			# Although we know it exists because of the ttl, 
+			# it is possible that when we get to loadUserData it has expired. 
+			# So we check the return value of loadUserData to avoid errors with json.dumps()
+		newData = self.loadUserData(token)
+		if newData == -1:
+			return None
+		newData.update(kwargs)
+		
+		# Saves the remaining pttl of the session
+		# If it is -1 it means that it did not exist
+		pttlSesion = self.sesion.pttl(token)
+		if pttlSesion==-1:
+			return None
+		
+		# Update user data with newData
+		self.sesion.set(token,json.dumps(newData))
+		self.sesion.pexpire(token,pttlSesion)
+
+	def loginToken(self,token) -> int :
+		# Collect the 'privilege' field of the session. 
+		# If it does not exist, returns -1
+		res = self.sesion.get(token)
+		if res is None:
+			return -1
+		return int(json.loads(res)['privilege'])
+
+	def _filter(self, **kwargs) -> bool:
+		args_set = set(kwargs.keys())
+
+		# with set theory we check that |kwargs ∩ data_keys|=|data_keys|
+		#if (len(args_set & self.data_keys) != len(self.data_keys)):
+		if(not args_set.issubset(self.data_keys)):
+			raise Exception('missing required variables on argument')
+		return True
+
+	@classmethod
+	def _init_class(cls):
+		cls.sesion = redis.Redis(host=rdK.DB_ADDRESS, port= rdK.DB_PORT, db=rdK.DB_SESION_INDEX, charset="utf-8", decode_responses=True)
+		cls.sesion.config_set(rdK.REDIS_MAXMEM_STR, rdK.MAXMEM)
+		cls.sesion.config_set(rdK.REDIS_POLICY_STR, rdK.POLICY)
+
+
+if __name__ == '__main__':
+	s = Sesion()
+	sesion = s.createSesion(username='Paco123',name='Francisco',password='1a2b3c',privilege=random.randrange(7))
+	print(f'sesion: ',sesion)
+	print(f'datos usuario (en el objeto s): ',s.verDatosUsuario())
+	print(f'privilege: ',s.loginToken(sesion['token']))
+	s.updateUserData(token=sesion['token'],username='Pepe33',name='Jose')
+	print(f'user data (loaded): ',s.loadUserData(sesion['token']))
+	print(f'datos usuario despues de load-rlo (en el objeto s): ',s.verDatosUsuario())
+	
