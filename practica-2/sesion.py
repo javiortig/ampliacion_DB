ae35d6f716b48fa2c4c0a67fa73743351d65f574
Ekaitz723 <52826804+Ekaitz723@users.noreply.github.com>
2021-11-09 20:38:24 +0100

    si

--- practica-2/sesion.py
+++ practica-2/sesion.py
@@ -15,7 +15,7 @@ class Sesion:
 	def createSesion(self,**kwargs) -> Union[dict,None] :
 		# Add 'privilege' if does not have it and check that the data is correct before dumping it in self
 		if 'privilege' not in kwargs.keys():
-			kwargs['privilege'] = 0
+			kwargs['privilege'] = rdK.DEFAULT_TICKET_PRIORITY
 		self._filter(**kwargs)
 		self.__dict__.update(kwargs)
 
@@ -35,13 +35,14 @@ class Sesion:
 
 	# Check local data
 	def verDatosUsuario(self) -> Union[dict,None] :
-		#solo se comprueba username ya que si esta username, tambien estan el resto
+		# Only 'username' is checked. In case that 'username' is present, the rest are also present
 		if 'username' in self.__dict__:
 			return {k:v for k,v in self.__dict__.items() if k in self.data_keys}
 		return None
 
 	def loadUserData(self,token):
-		# Loads user data from the server and dumps it into self
+		# Loads user data from the server and dumps it into self.
+		# Returns -1 if does not exist.
 		res = self.sesion.get(token)
 		if res is None:
 			return -1
@@ -51,34 +52,35 @@ class Sesion:
 		return userData
 
 	def updateUserData(self,token="",**kwargs) -> None :
+		# Check the data
 		self._filter(**kwargs)
 		
 		# Checks that we have passed a correct token to it
 		if token=="":
 			return None
 
-		# Update the data, and put the ttl back in
-			# Although we know it exists because of the ttl, 
-			# it is possible that when we get to loadUserData it has expired. 
-			# So we check the return value of loadUserData to avoid errors with json.dumps()
+		# As all the data is not necessarily being updated, if any of the data is missing, 
+		# it is taken from the data we already have
 		newData = self.loadUserData(token)
 		if newData == -1:
 			return None
 		newData.update(kwargs)
 		
-		# Saves the remaining pttl of the session
+		# Saves the remaining pttl (ttl in milliseconds) of the session
 		# If it is -1 it means that it did not exist
 		pttlSesion = self.sesion.pttl(token)
 		if pttlSesion==-1:
 			return None
 		
-		# Update user data with newData
+		# Update user data with newData. 
+		# Added the pttl (ttl in milliseconds) that it had before the update, 
+		# since when updating the expire is gone.
 		self.sesion.set(token,json.dumps(newData))
 		self.sesion.pexpire(token,pttlSesion)
 
 	def loginToken(self,token) -> int :
 		# Collect the 'privilege' field of the session. 
-		# If it does not exist, returns -1
+		# If it does not exist the session, returns -1
 		res = self.sesion.get(token)
 		if res is None:
 			return -1
@@ -87,8 +89,8 @@ class Sesion:
 	def _filter(self, **kwargs) -> bool:
 		args_set = set(kwargs.keys())
 
-		# with set theory we check that |kwargs ∩ data_keys|=|data_keys|
-		#if (len(args_set & self.data_keys) != len(self.data_keys)):
+		# If args_set is not subset of data_keys, i.e.,
+		# not all items in args_set exists in data_keys, raise expeption.
 		if(not args_set.issubset(self.data_keys)):
 			raise Exception('missing required variables on argument')
 		return True
