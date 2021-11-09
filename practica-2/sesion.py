4c4b22ec9e7b7a71ce1dd586d57030cd8748ab6e
Ekaitz723 <52826804+Ekaitz723@users.noreply.github.com>
2021-11-09 07:16:29 +0100

    ups, version antigua. Actualizo

--- practica-2/sesion.py
+++ practica-2/sesion.py
@@ -1,5 +1,6 @@
 import redis
 import json
+import secrets
 from typing import Union
 import constants.redis as rdK
 
@@ -7,22 +8,25 @@ class Sesion:
     data_keys = ['username','name','password','privilege']
     sesion = None
 
-    def _crearSesion(self,sobreescribir=False,**kwargs) -> dict :
+    def createSesion(self,privilege=0,**kwargs) -> Union[dict,None] :
+        # Add 'privilege' and check that the data is correct before dumping it in self
+        kwargs['privilege'] = privilege
         self._filter(**kwargs)
-
-        sesion_data = kwargs
-        sesion_data['privilege'] = 123
-        token = '1d84n1sd491ds7k'
-
-        #TODO si ya existe que no haga nada?
-        if sobreescribir or 0 == self.sesion.exists(token):
-            pipeline = redis.pipeline()
-            pipeline.set(token,json.dumps(sesion_data))
-            pipeline.ttl(token)
-            pipeline.execute()
+        self.__dict__.update(kwargs)
         
-        self.__dict__.update(sesion_data)
-        return sesion_data
+        #Create the token
+        token = secrets.token_hex(16)
+
+        # It saves it on the redis server. If it already existed, it does not change it. 
+        res = self.sesion.setnx(token,json.dumps({k:v for k,v in self.__dict__.items() if k in self.data_keys}))
+        
+        # In case it already exists, it returns -1. 
+        if res == 0:
+            return 1
+        # If it exists, it returns token and privilege.
+        else:
+            self.sesion.expire(token,rdK.SESION_EXPIRATION_TIME)
+            return {'token':token, 'privilege':self.__dict__['privilege']}
         
     #Para comprobar que funciona bien despues borrar(?)
     def verDatosUsuario(self) -> Union[dict,None] :
@@ -30,22 +34,36 @@ class Sesion:
         if 'username' in self.__dict__:
             return {k:v for k,v in self.__dict__.items() if k in self.data_keys}
         return None
+    
+    def loadUserData(self,token):
+        # Loads user data from the server and dumps it into self
+        userData = json.loads(self.sesion.get(token))
+        self.__dict__.update(userData)
 
-    def actualizarDatosUsuario(self,**kwargs) -> None :
-        args_set = set(kwargs.keys())
+        return userData
 
-        # rellenamos los datos que noe estamos actualizando con los que tenemos
-        if args_set.issubset(self.data_keys): # and (self.data_keys - args_set) in self.__dict__
-            for k in (self.data_keys - args_set):
-                kwargs[k] = self.__dict__[k]
+    def updateUserData(self,token="",**kwargs) -> None :
+        # Checks that we have passed a correct token to it
+        if token=="":
+            return None
 
-        self._crearSesion(sobreescribir=True,**kwargs)
-
-    def loginNuevo(self,**kwargs) -> dict :
-        return self._crearSesion(**kwargs)
+        # Saves the remaining ttl of the session
+        # If it is -1 it means that it did not exist
+        ttlSesion = self.sesion.ttl(token)
+        if ttlSesion==-1:
+            return None
+        
+        # Update the data, and put the ttl back in
+        self.sesion.set(token,self.loadUserData())
+        self.sesion.expire(token,ttlSesion)
     
     def loginToken(self,token) -> int :
-        return self.sesion.get(token,'privilege')
+        # Collect the 'privilege' field of the session. 
+        # If it does not exist, returns -1
+        res = self.sesion.get(token,'privilege')
+        if res is None:
+            res = -1
+        return int(res)
 
     def _filter(self, **kwargs) -> bool:
         args_set = set(kwargs.keys())
@@ -57,9 +75,6 @@ class Sesion:
 
     @classmethod
     def _init_class(cls):
-        # cls.sesion = redis.Redis(host=redisK.DB_ADDRESS, port= redisK.DB_PORT, db=redisK.DB_INDEX)
-        # cls.sesion.config_set(redisK.REDIS_MAXMEM_STR, redisK.MAXMEM)
-        # cls.sesion.config_set(redisK.REDIS_POLICY_STR, redisK.POLICY)
-        cls.sesion = redis.Redis(host=rdK.DB_ADDRESS, port= rdK.DB_PORT, db=rdK.SESION_INDEX)
+        cls.sesion = redis.Redis(host=rdK.DB_ADDRESS, port= rdK.DB_PORT, db=rdK.DB_SESION_INDEX)
         cls.sesion.config_set(rdK.REDIS_MAXMEM_STR, rdK.MAXMEM)
         cls.sesion.config_set(rdK.REDIS_POLICY_STR, rdK.POLICY)
