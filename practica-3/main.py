47cbc49a225f7d7db51b7351cd0b16ecdc137ac0
Ekaitz723 <52826804+Ekaitz723@users.noreply.github.com>
2021-12-01 23:45:36 +0100

    actualizo final (main)

--- practica-3/main.py
+++ practica-3/main.py
@@ -3,11 +3,15 @@ from Models.Redes import Redes
 
 if __name__ == "__main__":
     db = Redes()
-    user1 = 'Miguel V'
-    user2 = 'Andy Q'
-    user3 = 'Universidad Rey Juan Carlos (URJC)'
-    user4 = 'Josefa Valdes'
-    user5 = 'Angela G'
+    user1 = 'Margarita F'
+    user2 = 'Juana C'
+    user3 = 'Andy P'
+    user4 = 'Juan Z'
+    user5 = 'Josefa Johnson'
+    userS1 = "Joe Valdes"
+    userT1 = "Joe G"
+    userS2 = "Universidad Rey Juan Carlos (URJC)"
+    userT2 = "Angela Smith"
     distance = 4
     min_messages = 1
 
@@ -15,16 +19,37 @@ if __name__ == "__main__":
     db.query('match (n) detach delete n')
     # ../vs/python_proyects/jotasones/data.json
     db.query(db.load_json("ampliacion-db/ampliacion_DB/practica-3/superdata.json"))
+    # db.query(db.load_json("superdata.json"))
 
     db.query(db.create_publication(user5, "2019-08-12T00:00:01[Europe/Madrid]", "Soy guapa", "Asi soy asi soy jeje", [user1, user4]))
-    db.query(db.create_publication(user4, "2019-08-12T00:00:01[Europe/Madrid]", "Josefa soy sin mencion", "Asi soy asi soy jeje"))
+    db.query(db.create_publication(user4, "2019-08-12T00:00:01[Europe/Madrid]", "Juan soy sin mencion", "Asi soy asi soy jeje"))
     db.query(db.create_company("Applesitos"))
     db.query(db.create_company("Googlesitos", Ricos="Si", guapos="También"))
     db.query(db.create_user("Pablo Ramos", Admin="Si"))
-    db.query(db.create_university("Universidad inventada", Admin="Si"))
+    db.query(db.create_university("TBU", Admin="Si"))
     db.query(db.create_university("UUU"))
     db.query(db.delete_user("UUU"))
 
+    db.query(db.new_message_to_str("Hola guapeton", "2019-08-12T00:00:01[Europe/Madrid]", user1, user2))
+    db.query(db.new_message_to_str("Que pasa juani", "2020-08-12T00:00:01[Europe/Madrid]", user2, user1))
+    db.query(db.new_message_to_str("Hola guapeton segundo", "2021-08-12T00:00:01[Europe/Madrid]", user1, user2))
+
+    db.query(db.new_message_to_str("hola 1", "2019-08-12T00:00:01[Europe/Madrid]", userS2, user3))
+    db.query(db.new_message_to_str("hola 2", "2019-08-12T00:00:01[Europe/Madrid]", userS2, user3))
+    db.query(db.new_message_to_str("hola 3", "2019-08-12T00:00:01[Europe/Madrid]", user3 , userS1))
+    db.query(db.new_message_to_str("hola 4", "2019-08-12T00:00:01[Europe/Madrid]", user3, userS2))
+
+
+    db.query(db.new_message_to_str("Hola guapeton tercero", "2019-08-12T00:00:01[Europe/Madrid]", userS1, userT1))
+    db.query(db.new_message_to_str("Hola guapeton tercero soy simp", "2019-08-12T00:00:01[Europe/Madrid]", userT1, userS1))
+    db.query(db.new_message_to_str("Hola guapeton tercero soy simp2", "2019-08-12T00:00:01[Europe/Madrid]", userT1, userS1))
+    db.query(db.new_message_to_str("a", "2019-08-12T00:00:01[Europe/Madrid]", userS2, userT2))
+    db.query(db.new_message_to_str("b", "2019-08-12T00:00:01[Europe/Madrid]", userS2, userT2))
+    db.query(db.new_message_to_str("c", "2019-08-12T00:00:01[Europe/Madrid]", userT2, userS2))
+    db.query(db.new_message_to_str("d", "2019-08-12T00:00:01[Europe/Madrid]", userT2, userS2))
+    db.query(db.new_message_to_str("e", "2019-08-12T00:00:01[Europe/Madrid]", userT2, userS2))
+    db.query(db.new_message_to_str("f", "2019-08-12T00:00:01[Europe/Madrid]", userT2, userS2))
+    db.query('match (a:user:basic {username:"'+user5+'"})-[:publishes]->(:publishing)-[:mention]->(b) create (a)-[w:work]->(b)')
 
     print("Friends and Family of user: " + user1)
     Q1 = db.query(db.friend_and_family_to_str(user1))
@@ -37,7 +62,7 @@ if __name__ == "__main__":
 
     print("\n\n\n\n")
     print("Messages after date between users: " + user1 + " and " + user2)
-    Q3 = db.query(db.messages_after_date_to_str(username_a=user1, username_b=user2,datetime="2005-9-25T22:43:15[Europe/Madrid]"))
+    Q3 = db.query(db.messages_after_date_to_str(username_a=user1, username_b=user2,datetime="2019-09-11T22:43:15[Europe/Madrid]"))
     print(Q3)
 
     print("\n\n\n\n")
@@ -46,8 +71,8 @@ if __name__ == "__main__":
     print(Q4)
 
     print("\n\n\n\n")
-    print("Users mentioned in a plublishing from user " + user3 + " that have a work relationship")
-    Q5 = db.query(db.mentioned_users_with_laboral_relation(username=user3))
+    print("Users mentioned in a plublishing from user " + user5 + " that have a work relationship")
+    Q5 = db.query(db.mentioned_users_with_laboral_relation(username=user5))
     print(Q5)
 
     print("\n\n\n\n")
@@ -55,28 +80,10 @@ if __name__ == "__main__":
     Q6 = db.query(db.distance_new_relationships(distance=distance, username=user4))
     print(Q6)
 
-
-    db.query(db.new_message_to_str("Hola guapeton", "2019-08-12T00:00:01[Europe/Madrid]", "Angela G", "Meri C"))
-    db.query(db.new_message_to_str("Que pasa juani", "2019-08-12T00:00:01[Europe/Madrid]", "Meri C", "Juan X"))
-
-    db.query(db.new_message_to_str("Hola guapeton segundo", "2019-08-12T00:00:01[Europe/Madrid]", "Angela G", "Joe Orti, Garcia"))
-    db.query(db.new_message_to_str("hola 1", "2019-08-12T00:00:01[Europe/Madrid]", "Joe Orti, Garcia", "University of Vigo"))
-    db.query(db.new_message_to_str("hola 2", "2019-08-12T00:00:01[Europe/Madrid]", "Joe Orti, Garcia", "University of Vigo"))
-    db.query(db.new_message_to_str("hola 3", "2019-08-12T00:00:01[Europe/Madrid]", "Joe Orti, Garcia", "University of Vigo"))
-    db.query(db.new_message_to_str("hola 4", "2019-08-12T00:00:01[Europe/Madrid]", "Joe Orti, Garcia", "University of Vigo"))
-
-    db.query(db.new_message_to_str("Hola guapeton tercero", "2019-08-12T00:00:01[Europe/Madrid]", "Angela G", "John Smith"))
-    db.query(db.new_message_to_str("Hola guapeton tercero soy simp", "2019-08-12T00:00:01[Europe/Madrid]", "Angela G", "John Smith"))
-    db.query(db.new_message_to_str("Hola guapeton tercero soy simp2", "2019-08-12T00:00:01[Europe/Madrid]", "Angela G", "John Smith"))
-    db.query(db.new_message_to_str("a", "2019-08-12T00:00:01[Europe/Madrid]", "John Smith", "lhkyasol"))
-    db.query(db.new_message_to_str("b", "2019-08-12T00:00:01[Europe/Madrid]", "John Smith", "lhkyasol"))
-    db.query(db.new_message_to_str("c", "2019-08-12T00:00:01[Europe/Madrid]", "John Smith", "lhkyasol"))
-    db.query(db.new_message_to_str("d", "2019-08-12T00:00:01[Europe/Madrid]", "John Smith", "lhkyasol"))
-    db.query(db.new_message_to_str("e", "2019-08-12T00:00:01[Europe/Madrid]", "John Smith", "lhkyasol"))
-    db.query(db.new_message_to_str("f", "2019-08-12T00:00:01[Europe/Madrid]", "John Smith", "lhkyasol"))
     print("\n\n\n\n")
-    print("New relationships for user " + user5 + "based on" + str(min_messages) +  "message interactions")
-    Q7 = db.query(db.messages_new_relationships(user1,min_messages=min_messages))
+    print("New relationships for user " + user3 + " based on " + str(min_messages) +  " message interactions")
+    print(db.messages_new_relationships(user3,min_messages=min_messages))
+    Q7 = db.query(db.messages_new_relationships(user3,min_messages=min_messages))
     print(Q7)
 
     
