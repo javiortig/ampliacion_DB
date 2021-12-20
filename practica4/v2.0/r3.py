f3c0fbf43e5c4934e2374e0d969902d26b72bb18
Ekaitz723 <52826804+Ekaitz723@users.noreply.github.com>
2021-12-20 03:43:34 +0100

    muevo el sacar la suma total al bucle de arriba
    nosesiestarabien

--- practica4/v2.0/r3.py
+++ practica4/v2.0/r3.py
@@ -3,16 +3,18 @@
 import sys
 
 counts = {}
+total = 0 #
 for line in sys.stdin:
     line = line.replace('\n', '')
     if (line in counts):
         counts[line] += 1
     else:
         counts[line] = 1
+    total += 1 #
 
-total = 0
-for v in counts.values():
-    total += v
+# total = 0
+# for v in counts.values():
+#     total += v
 
 for k in counts:
     counts[k] = str(counts[k]/total * 100) + ' %'    
